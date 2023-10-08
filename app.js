9c0855ff7ce8043d4ab28fc537eaa13cd09fdd68
aureliass <143623373+aureliass@users.noreply.github.com>
2023-10-08 17:06:22 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -24,3 +24,11 @@ routes.forEach((route) => app.use(route));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+app.listen(PORT, () => console.log('Server ready on port:', PORT));
