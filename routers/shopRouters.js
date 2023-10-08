430d611810e1d6c84bc134139d21bfdb498f345b
Theo Gracia Kalasuso <141816579+Theogracia@users.noreply.github.com>
2023-10-08 14:57:03 +0800

    Delete routers directory

--- routers/shopRouters.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const shopController = require('../controllers/shopController');
-const shopRoutes = express.Router();
-
-
-shopRoutes.get('/', shopController.getItems);
-
-
-module.exports = { shopRoutes };
