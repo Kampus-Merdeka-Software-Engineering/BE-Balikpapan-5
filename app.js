8af5b239cad342c6d14feb6a047a0934972f1d08
aureliass <143623373+aureliass@users.noreply.github.com>
2023-10-08 21:18:56 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -32,3 +32,23 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => console.log('Server ready on port:', PORT));
+
+
+
+async function loadProductDetail () {
+  let id = sessionStorage.getItem('product_id') ;
+  let response = await fetch('https://be-balikpapan-5-production.up.railway.appp/api/production');
+  let data = await response.json();
+  data = data.data;
+}
+
+
+fetch('https://be-balikpapan-5.railway.app/api/data')
+  .then(response => response.json())
+  .then(data => {
+    // Lakukan sesuatu dengan data yang diterima dari backend
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Terjadi kesalahan:', error);
+  });
