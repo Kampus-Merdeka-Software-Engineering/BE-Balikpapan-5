564b392b70607e6cc3ce7e0d9c47c8973ae225fd
aureliass <143623373+aureliass@users.noreply.github.com>
2023-10-09 11:19:52 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const router = express.Router();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,36 +17,13 @@ app.use((req, res, next) => {
   next();
 });
 
-routes.forEach((route) => app.use(route));
+app.use('/api', routes); // Menggunakan '/api' sebagai awalan untuk semua rute
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal server error' });
 });
-
-app.listen(PORT, () => console.log('Server ready on port:', PORT));
-
-
-
-async function loadProductDetail () {
-  let id = sessionStorage.getItem('product_id') ;
-  let response = await fetch('https://be-balikpapan-5-production.up.railway.appp/api/production');
-  let data = await response.json();
-  data = data.data;
-}
-
-
-fetch('https://be-balikpapan-5.railway.app/api/data')
-  .then(response => response.json())
-  .then(data => {
-    // Lakukan sesuatu dengan data yang diterima dari backend
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Terjadi kesalahan:', error);
-  });
